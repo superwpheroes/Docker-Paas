4f7b91b1906d986e005c332c526b2e79bef5f829
Marius <marius@superwpheroes.io>
2022-02-07 18:42:45 +0200

    dockerised lf

---
+++ src/wp-config.php
@@ -0,0 +1,99 @@
+<?php
+
+
+
+
+ // Added by WP Rocket
+ // Added by WP Rocket
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+ //Added by WP-Cache Manager
+// define( 'WPCACHEHOME', '/home/lifefram/public_html/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define('DB_NAME', getenv('DB_NAME'));
+/** MySQL database username */
+define('DB_USER', getenv('DB_USER'));
+/** MySQL database password */
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+/** MySQL hostname */
+define('DB_HOST', getenv('DB_HOST'));
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', getenv('DB_CHARSET'));
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', getenv('DB_COLLATE'));
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
+/**#@-*/
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = getenv('TABLE_PREFIX');
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+//define('WP_DEBUG', true);
+ini_set('log_errors','On');
+ini_set('display_errors','On');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', false);
+define('WP_DEBUG_DISPLAY', true);
+define('WP_MEMORY_LIMIT', '800M');
+define( 'AUTOSAVE_INTERVAL', 300 );
+define( 'WP_POST_REVISIONS', 5 );
+define( 'EMPTY_TRASH_DAYS', 7 );
+define( 'WP_CRON_LOCK_TIMEOUT', 120 );
+
+// define( 'POST_SMTP_AUTH_USERNAME', 'info@life-framer.com');
+// define( 'POST_SMTP_AUTH_PASSWORD', 'hF62t4ZGgp');
+
+/* That's all, stop editing! Happy blogging. */
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
